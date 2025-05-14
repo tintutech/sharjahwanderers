1840f809b59ddcf6497e6d78d0b9acdc3bc29a90
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-14 13:42:18 +0400

    MainContents.jsx

--- src/MainContents.jsx
+++ src/MainContents.jsx
@@ -25,6 +25,45 @@ const data = {
 	},
 };
 
+const aboutData = {
+	title: "About the Club",
+	content:
+		"Consectetur dolorem ipsa dolorum explicabo dolor. Officiis aliquam adipisicing sequi officia obcaecati quaerat Doloribus dicta omnis facere eum sint Dicta atque impedit quisquam sunt odio Natus aspernatur ullam odit quis",
+	link: "",
+	list: ["Fully licensed bar", "Swimming pool", "Spa and Salon"],
+};
+
+const guestBook = {
+	para1: "Amazing staff, exellent trainers.",
+	para2: "MEMBER",
+	para3:
+		"Consectetur animi ipsam aut cumque accusantium Deserunt sint porro minima eos optio. Maiores est ex qui dolorum quasi! Nulla quia accusamus quibusdam perferendis tempore itaque.",
+	name: "MARK S.",
+};
+
+function About() {
+	return (
+		<div id="about">
+			<div className="leftCard">
+				<div>
+					<h3>{aboutData.title}</h3>
+					<p>{aboutData.content}</p>
+					<a href="">Learn More</a>
+				</div>
+				<div>
+					<ul>
+						<li>{aboutData.list[0]}</li>
+						<li>{aboutData.list[1]}</li>
+						<li>{aboutData.list[2]}</li>
+					</ul>
+				</div>
+			</div>
+
+			<div className="rightCard"></div>
+		</div>
+	);
+}
+
 function Experience() {
 	return (
 		<div id="experience">
@@ -69,8 +108,23 @@ function Events() {
 		</div>
 	);
 }
+
 function Guestbook() {
-	return <div id="guestbook"></div>;
+	return (
+		<div id="guestbook">
+			<div className="left">
+				<h3>GUESTBOOK</h3>
+				<div>
+					<p>{guestBook.para1}</p>
+					<p>{guestBook.para2}</p>
+					<p>{guestBook.para3}</p>
+
+					<h3 className="guestName">{guestBook.name}</h3>
+				</div>
+				<p>Learn More about our Membership benefits</p>
+			</div>
+		</div>
+	);
 }
 
 export default function MainContents() {
@@ -78,8 +132,7 @@ export default function MainContents() {
 		<>
 			<div className="mainContainer">
 				<h1>EXPLORE</h1>
-
-				<div className="explore"></div>
+				<About />
 				<Experience />
 				<Events />
 				<Guestbook />
