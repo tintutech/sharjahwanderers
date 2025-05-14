310737d7c3eea0dc3563020ea0ba613eeb3ca4a0
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-14 13:52:10 +0400

    Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,54 @@
+function LeftContents() {
+	return (
+		<div className="footerLeft">
+			<img />
+			<p className="footerBold"></p>
+			<div>
+				<p>Samman,</p>
+				<p>Harwan Suburb,</p>
+				<p>Sharjah,</p>
+				<p>United Arab Emirates</p>
+			</div>
+			<p>
+				<span className="footerBold">TEL:</span> +97100000000
+			</p>
+			<p>
+				<span className="footerBold">EMAIL:</span> info@sharjahwac.com
+			</p>
+		</div>
+	);
+}
+
+function RightContents() {
+	return (
+		<div className="footerRight">
+			<div>
+				<a href="#">Home</a>
+				<a href="#">Salon & Spa</a>
+				<a href="#">Membership</a>
+				<a href="#">Restaurant</a>
+				<a href="#">Events</a>
+				<a href="#">Offers</a>
+				<a href="#">Contact</a>
+			</div>
+			<div>
+				<a href="#">Facilities</a>
+				<a href="#">Home</a>
+				<a href="#">Home</a>
+				<a href="#">Home</a>
+				<a href="#">Home</a>
+				<a href="#">Home</a>
+				<a href="#">Home</a>
+			</div>
+		</div>
+	);
+}
+
 export default function Footer() {
 	return (
 		<div className="footer">
-			<h1>Footer</h1>
+			<LeftContents />
+			<RightContents />
 		</div>
 	);
 }
