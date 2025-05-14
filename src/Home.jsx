2888c1df795bb1df05ef748220afb9394c925c9e
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-14 11:52:56 +0400

    progress

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,13 @@
 import HeroNav from "./HeroNav.jsx";
 import MainContents from "./MainContents.jsx";
+import Footer from "./Footer.jsx";
 
 export default function Home() {
 	return (
 		<div className="heroNav">
 			<HeroNav />
 			<MainContents />
+			<Footer />
 		</div>
 	);
 }
