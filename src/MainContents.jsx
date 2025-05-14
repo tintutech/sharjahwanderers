27187b156a1e943e01fdf38e1adeea429933abbc
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-14 20:04:36 +0400

    progress

--- src/MainContents.jsx
+++ src/MainContents.jsx
@@ -2,25 +2,22 @@ const data = {
 	cardOne: {
 		title: "EAT",
 		paraOne:
-			"Consectetur amet eos minima ratione quis? Dolorem officia ad nobis sequi laboriosam aliquam Vel adipisci harum cumque dolor nulla Error a cumque doloremque nisi ipsum? Lorem ducimus vitae non maxime",
-		paraTwo:
-			"Adipisicing vero rem ab et error quaerat Doloremque minus tempore.",
+			"Consectetur amet eos minima ratione quis? Dolorem officia ad nobis sequi laboriosam ",
+		paraTwo: "Adipisicing vero rem ab et error quaerat Dole.",
 		link: "",
 	},
 	cardTwo: {
 		title: "PLAY",
 		paraOne:
-			"Amet enim deserunt itaque voluptatibus at Maiores error animi ipsa consequatur nostrum consequatur eius voluptas? Rerum quas quisquam sunt voluptatem dolor Asperiores natus pariatur odit nesciunt aliquid Optio beatae dolorum",
-		paraTwo:
-			"Consectetur quasi quibusdam placeat delectus quas illum, mollitia Officia nobis",
+			"Amet enim deserunt itaque voluptatibus at Maiores error animi ipsa consequatur nostrum",
+		paraTwo: "Consectetur quasi quibusdam delectus",
 		link: "",
 	},
 	cardThree: {
 		title: "Indulge",
 		paraOne:
-			"Ipsum cum labore maiores dolorem nostrum, veniam. Deserunt odio eaque consectetur autem inventore aspernatur In modi reprehenderit iste recusandae delectus laudantium deserunt Molestiae distinctio facere optio nesciunt labore omnis Eaque!",
-		paraTwo:
-			"Consectetur doloremque nihil numquam eaque ut. Similique esse sint esse?",
+			"Ipsum cum labore maiores dolorem nostrum, veniam. Deserunt odio consectetur autem inventore",
+		paraTwo: "Consectetur doloremque nihil Similique?",
 		link: "",
 	},
 };
@@ -71,21 +68,28 @@ function Experience() {
 			<h2>EXPERIENCE</h2>
 			<div className="cardContainer">
 				<div className="card">
-					<h2>{data.cardOne.title}</h2>
-					<p>{data.cardOne.paraOne}</p>
-					<p>{data.cardOne.paraTwo}</p>
+					<div>
+						<h2>{data.cardOne.title}</h2>
+						<p>{data.cardOne.paraOne}</p>
+						<p>{data.cardOne.paraTwo}</p>
+					</div>
 					<a href="#">EXPLORE</a>
 				</div>
 				<div className="card">
-					<h2>{data.cardTwo.title}</h2>
-					<p>{data.cardTwo.paraOne}</p>
-					<p>{data.cardTwo.paraTwo}</p>
+					<div>
+						<h2>{data.cardTwo.title}</h2>
+						<p>{data.cardTwo.paraOne}</p>
+						<p>{data.cardTwo.paraTwo}</p>
+					</div>
 					<a href="#">EXPLORE</a>
 				</div>
 				<div className="card">
-					<h2>{data.cardThree.title}</h2>
-					<p>{data.cardThree.paraOne}</p>
-					<p>{data.cardThree.paraTwo}</p>
+					<div>
+						{" "}
+						<h2>{data.cardThree.title}</h2>
+						<p>{data.cardThree.paraOne}</p>
+						<p>{data.cardThree.paraTwo}</p>
+					</div>{" "}
 					<a href="#">EXPLORE</a>
 				</div>
 			</div>
