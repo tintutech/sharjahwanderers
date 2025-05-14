04487a4741e407fa184301eae60dcd91cfcd6fd7
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-14 10:45:25 +0400

    MainContents.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,11 @@
 import HeroNav from "./HeroNav.jsx";
+import MainContents from "./MainContents.jsx";
 
 export default function Home() {
 	return (
 		<div className="heroNav">
 			<HeroNav />
+			<MainContents />
 		</div>
 	);
 }
