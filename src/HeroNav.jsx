2acbfd6c1a281950c34530709bf4fd9437631f3f
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-14 10:42:35 +0400

    HeroNav.jsx

---
+++ src/HeroNav.jsx
@@ -0,0 +1,33 @@
+export default function Nav() {
+	return (
+		<>
+			<div className="navBar">
+				<ul>
+					<li>one</li>
+					<li>two</li>
+					<li>three</li>
+					<li>four</li>
+					<li>five</li>
+					<li>six</li>
+				</ul>
+			</div>
+
+			<div className="heroSection">
+				<div className="heroLeft">
+					<h2>
+						A private <span class="heroSpecial">Oasis</span>, for all wanderers
+						in Sharjah.
+					</h2>
+					<p>
+						Adipisicing maxime eligendi quod blanditiis consectetur animi?
+						Exercitationem obcaecati quaerat saepe velit sapiente Quibusdam
+						incidunt totam lorem enim facilis Dicta vero voluptas ut vitae quia
+					</p>
+					<a href="#">Become a member</a>
+					<a href="#">Explore offers, or attend an event?</a>
+				</div>
+				<div className="heroRight"></div>
+			</div>
+		</>
+	);
+}
