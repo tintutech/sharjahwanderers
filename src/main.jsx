63a9d022e43665d1cc287d5f7bc485728fcffa4e
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-19 17:08:09 +0400

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Home from "./Home.jsx";
-import AboutMembership from "./pages/aboutMembership/AboutMembership.jsx";
+
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<AboutMembership />
+		<Home />
 	</StrictMode>
 );
