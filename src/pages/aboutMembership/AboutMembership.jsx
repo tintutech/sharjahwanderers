8b82786991e2e1b93d1532c472ede91ee72e45ec
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-19 08:58:02 +0400

    progress

---
+++ src/pages/aboutMembership/AboutMembership.jsx
@@ -0,0 +1,11 @@
+import Nav from "../../components/Nav.jsx";
+import Footer from "../../components/Footer.jsx";
+
+export default function AboutMembership() {
+	return (
+		<>
+			<Nav />
+			<Footer />
+		</>
+	);
+}
