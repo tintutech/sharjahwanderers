05aa3bf4d9311c048c218dd2d5c4e6cf632ef052
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-20 16:24:21 +0400

    About.jsx

--- src/pages/aboutMembership/About.jsx
+++ src/pages/aboutMembership/About.jsx
@@ -1,13 +1,14 @@
+import "./aboutStyles.css";
+
 export default function About() {
 	return (
 		<div id="about">
-			<h3>ABOUT THE CLUB</h3>
+			<h1>ABOUT THE CLUB</h1>
 			<div className="rightContent">
-				<h2>Our Story so far</h2>
+				<h2>Our Story so far...</h2>
 				<p>
 					Founded in <span className="dateEm">1977</span>
-				</p>
-				<p>
+					<br />
 					The Sharjah Wanderers Sports club, was established by a group of
 					expats who required a rugby pitch and community center.
 				</p>
@@ -15,13 +16,13 @@ export default function About() {
 					The name “Sharjah Wanderers”, derived from the initial use for Rugby
 					games, and commonly used moniker by the Rugby teams in England who go
 					by “Wanderers” when representing the team.
-					<br />
-					Today, the club is home to the Sharjah Wanderers Rugby team.
 				</p>
+				<p>Today, the club is home to the Sharjah Wanderers Rugby team.</p>
 				<p>
 					Sharjah Wanderers Sports Club, includes the only licensed wet bar
 					facility at their Clubhouse, in Sharjah.
 				</p>
+				<a href="#">WIKI</a>
 			</div>
 		</div>
 	);
