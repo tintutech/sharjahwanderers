f19cef54c004ee22b972357bc34f59230df5ece9
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-20 15:43:01 +0400

    About.jsx

---
+++ src/pages/aboutMembership/About.jsx
@@ -0,0 +1,28 @@
+export default function About() {
+	return (
+		<div id="about">
+			<h3>ABOUT THE CLUB</h3>
+			<div className="rightContent">
+				<h2>Our Story so far</h2>
+				<p>
+					Founded in <span className="dateEm">1977</span>
+				</p>
+				<p>
+					The Sharjah Wanderers Sports club, was established by a group of
+					expats who required a rugby pitch and community center.
+				</p>
+				<p>
+					The name “Sharjah Wanderers”, derived from the initial use for Rugby
+					games, and commonly used moniker by the Rugby teams in England who go
+					by “Wanderers” when representing the team.
+					<br />
+					Today, the club is home to the Sharjah Wanderers Rugby team.
+				</p>
+				<p>
+					Sharjah Wanderers Sports Club, includes the only licensed wet bar
+					facility at their Clubhouse, in Sharjah.
+				</p>
+			</div>
+		</div>
+	);
+}
