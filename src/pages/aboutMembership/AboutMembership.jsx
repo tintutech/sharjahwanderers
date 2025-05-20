917702b090e46805d878f21b5b1618582f1f28c9
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-20 15:42:52 +0400

    progress

--- src/pages/aboutMembership/AboutMembership.jsx
+++ src/pages/aboutMembership/AboutMembership.jsx
@@ -2,12 +2,14 @@ import "./aboutMembershipStyles.css";
 
 import Nav from "../../components/Nav.jsx";
 import Footer from "../../components/Footer.jsx";
+import About from "./About.jsx";
 
 export default function AboutMembership() {
 	return (
 		<>
 			<div id="topSection">
 				<Nav />
+				<About />
 			</div>
 			<Footer />
 		</>
