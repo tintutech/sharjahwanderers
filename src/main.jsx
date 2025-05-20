7955130daa4ac9867ac9f5d3ae1ec47066b166f7
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-20 19:20:26 +0400

    progress

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import Home from "./Home.jsx";
+import AboutMembership from "./pages/aboutMembership/AboutMembership.jsx";
 
+/*
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<Home />
 	</StrictMode>
 );
-
-import AboutMembership from "./pages/aboutMembership/AboutMembership.jsx";
-/*
+*/
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<AboutMembership />
 	</StrictMode>
 );
-
-*/
