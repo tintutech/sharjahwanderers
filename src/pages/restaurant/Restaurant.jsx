12c402701f9544a98944c216297c8f9e334ccb66
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-28 13:49:41 +0400

    Restaurant Page

---
+++ src/pages/restaurant/Restaurant.jsx
@@ -0,0 +1,14 @@
+import Nav from "/src/components/Nav.jsx";
+import Footer from "/src/components/Footer.jsx";
+
+export default function Restaurant() {
+	return (
+		<div id="restaurant">
+			<div className="top">
+				<Nav />
+			</div>
+
+			<Footer />
+		</div>
+	);
+}
