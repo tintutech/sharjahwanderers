b025e4796ed5a82190fc327acb96538e16d5b11e
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-28 20:01:52 +0400

    DineIn.jsx

--- src/pages/restaurant/DineIn.jsx
+++ src/pages/restaurant/DineIn.jsx
@@ -1,37 +1,38 @@
+import image1 from "/dineIn/image1.png";
+import image2 from "/dineIn/image2.png";
+import image3 from "/dineIn/image3.png";
+import image4 from "/dineIn/image4.png";
+import image5 from "/dineIn/image5.png";
+import image6 from "/dineIn/image6.png";
 
-
-import image1 from "/public/dineIn/image1.png";
-import image2 from "/public/dineIn/image2.png";
-import image3 from "/public/dineIn/image3.png";
-import image4 from "/public/dineIn/image4.png";
-import image5 from "/public/dineIn/image5.png";
-import image6 from "/public/dineIn/image6.png";
-
+import "./dineInStyles.css";
 
 let images = [image1, image2, image3, image4, image5, image6];
 
 function ReturnImages() {
+	let n = 0;
 	let imgReturn = images.map((e) => {
-		return ( <img src={e} width="800" />)
-	}
-)
-	return (
-		<>
-		{imgReturn}
-		</>
-	)
+		return <img onClick={(e) => expandedPage(e)} key={n++} src={e} />;
+	});
+	return <div className="galleryContainer">{imgReturn}</div>;
+}
+
+function expandedPage(e) {
+	console.log(e.target.parentNode.parentNode.querySelector(".dineInFull"));
 }
 
 export default function DineIn() {
 	return (
 		<div className="dineInSection">
+			<div className="dineInFull hidden">
+				<img src="/multiply.svg" />
+				<img className="expandedPage" />
+			</div>
 			<div className="dineInTop">
 				<h2>DINE-IN</h2>
 				<p>BOOK A TABLE?</p>
 			</div>
 			<ReturnImages />
 		</div>
-
-	)
-
-};
+	);
+}
