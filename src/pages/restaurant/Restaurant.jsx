434675b4cfe6dab2e927e79c1abc0ab57a2012ab
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-28 14:36:02 +0400

    Restaurant.jsx

--- src/pages/restaurant/Restaurant.jsx
+++ src/pages/restaurant/Restaurant.jsx
@@ -1,6 +1,10 @@
 import Nav from "/src/components/Nav.jsx";
 import Footer from "/src/components/Footer.jsx";
 
+function Hello(e) {
+	console.log(e.target.src);
+}
+
 export default function Restaurant() {
 	return (
 		<div id="restaurant">
@@ -8,11 +12,42 @@ export default function Restaurant() {
 				<Nav />
 			</div>
 			<object
+				onClick={() => Hello()}
 				className="pdf"
 				data="/public/dineIn.pdf"
 				width="800"
 				height="600"
 			></object>
+			<img
+				onClick={(e) => Hello(e)}
+				src="/public/dineIn/image1.png"
+				width="800"
+			/>
+			<img
+				onClick={(e) => Hello(e)}
+				src="/public/dineIn/image2.png"
+				width="800"
+			/>
+			<img
+				onClick={(e) => Hello(e)}
+				src="/public/dineIn/image3.png"
+				width="800"
+			/>
+			<img
+				onClick={(e) => Hello(e)}
+				src="/public/dineIn/image4.png"
+				width="800"
+			/>
+			<img
+				onClick={(e) => Hello(e)}
+				src="/public/dineIn/image5.png"
+				width="800"
+			/>
+			<img
+				onClick={(e) => Hello(e)}
+				src="/public/dineIn/image6.png"
+				width="800"
+			/>
 
 			<Footer />
 		</div>
