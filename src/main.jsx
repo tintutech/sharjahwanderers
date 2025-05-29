ffef93bb87a687f60a7337238a2adbdc0a5d3662
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-29 08:42:24 +0400

    changed color

--- src/main.jsx
+++ src/main.jsx
@@ -5,21 +5,19 @@ import Home from "./Home.jsx";
 import AboutMembership from "./pages/aboutMembership/AboutMembership.jsx";
 import Restaurant from "./pages/restaurant/Restaurant.jsx";
 
-/*
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<Home />
 	</StrictMode>
 );
+/*
 
-*/
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<AboutMembership />
 	</StrictMode>
 );
 
-/*
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<Restaurant />
