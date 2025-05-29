ff9ce2237b9dd64e68faa2143ac724e5475591f5
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-29 17:49:28 +0400

    Delivery.jsx

--- src/pages/restaurant/Delivery.jsx
+++ src/pages/restaurant/Delivery.jsx
@@ -1,3 +1,47 @@
+import image1 from "/delivery/image1.jpg";
+import image2 from "/delivery/image2.jpg";
+import image3 from "/delivery/image3.jpg";
+
+let images = [image1, image2, image3];
+
+function LeftSection() {
+	return (
+		<div className="leftSection">
+			<h2>DELIVERY</h2>
+			<div className="leftLower">
+				<p>DELIVERY TIMINGS 11:00 AM - 9:00 PM</p>
+				<p>Monday - FRIDAY</p>
+			</div>
+		</div>
+	);
+}
+
+function RightSection() {
+	return (
+		<div className="rightSection">
+			<h3>SUNDAY ROAST AVAILABLE</h3>
+			<a className="detailsBtn" href="#">
+				ASK FOR DETAILS
+			</a>
+		</div>
+	);
+}
+
+function ReturnImages() {
+	let n = 0;
+	let imgHTML = images.map((e) => {
+		return <img key={n++} src={e} />;
+	});
+	return <div className="galleryContainer">{imgHTML}</div>;
+}
+
 export default function Delivery() {
-	return <div className="deliverySection"></div>;
+	return (
+		<div className="deliverySection">
+			<div className="topSection">
+				<LeftSection />
+				<RightSection />
+			</div>
+		</div>
+	);
 }
