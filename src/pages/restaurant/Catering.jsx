ac3d04502e83874978cd94409a107bf60b2275f5
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-30 16:05:44 +0400

    Catering.jsx

---
+++ src/pages/restaurant/Catering.jsx
@@ -0,0 +1,35 @@
+let catering1 = {
+	p1: "We offer catering services for events hosted within Club premises* and private venues for events and parties of all sizes.",
+	p2: "From corporate events, birthday parties and sports events!",
+	p3: "All catering requests should be done atleast 72 hours before event date.",
+	downloadLink: "#",
+};
+
+let catering2 = {
+	p1: "Let one of our events representatives help plan your unique event with us!",
+	link: "#",
+	disclaimer: "*VENUE HIRE IS SUBJECT TO AVAILABILITY.",
+};
+
+export default function Catering() {
+	return (
+		<div className="catering">
+			<div className="rightContents">
+				<div>
+					<h2>CATERING</h2>
+					<p>{catering1.p1}</p>
+					<p>{catering1.p2}</p>
+					<p>{catering1.p3}</p>
+					<a href={catering1.downloadLink}>DOWNLOAD MENU</a>
+				</div>
+				<div>
+					<div className="topCard">
+						<p>{catering2.p1}</p>
+						<a href={catering2.link}>CATERING INQUIRY</a>
+					</div>
+					<p className="disclaimer">{catering2.disclaimer}</p>
+				</div>
+			</div>
+		</div>
+	);
+}
