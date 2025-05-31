4a78e180f40ebe28c2497fa221fa30e958c777cc
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-05-31 18:55:06 +0400

    CateringCarousel.jsx

--- src/pages/restaurant/CateringCarousel.jsx
+++ src/pages/restaurant/CateringCarousel.jsx
@@ -1,11 +1,3 @@
-import image1 from "/dineIn/image1.jpeg";
-import image2 from "/dineIn/image2.jpeg";
-import image3 from "/dineIn/image3.jpg";
-import image4 from "/dineIn/image4.jpg";
-import { useState } from "react";
-
-let images = [image1, image2, image3, image4];
-
 export default function CateringCarousel() {
 	return <img />;
 }
