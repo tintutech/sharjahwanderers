03813feaafc861b73bd81c1f526fe8838040b45d
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-06-12 08:18:31 +0400

    Facilities.jsx

---
+++ src/pages/facilities/Facilities.jsx
@@ -0,0 +1,12 @@
+import Nav from "/src/components/Nav.jsx";
+import Footer from "/src/components/Footer.jsx";
+
+export default function Facilities() {
+	return (
+		<div id="facilities">
+			<Nav />
+			<div className="mainContents"></div>
+			<Footer />
+		</div>
+	);
+}
