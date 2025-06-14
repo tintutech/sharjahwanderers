bb266a79cdeb93e916bf5a3f9a6548d084b85dd1
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-06-14 09:44:31 +0400

    SalonSpa.jsx

---
+++ src/pages/salonSpa/SalonSpa.jsx
@@ -0,0 +1,12 @@
+import Nav from "/src/components/Nav.jsx";
+import Footer from "/src/components/Footer.jsx";
+import "./salonSpaStyles.css";
+
+export default function SalonSpa() {
+	return (
+		<div id="salonspa">
+			<Nav />
+			<Footer />
+		</div>
+	);
+}
