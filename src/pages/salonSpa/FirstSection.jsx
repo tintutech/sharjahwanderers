04200619f63b8b7b4531615fb09a264cab2445a2
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-06-14 17:02:54 +0400

    FirstSection.jsx

---
+++ src/pages/salonSpa/FirstSection.jsx
@@ -0,0 +1,25 @@
+export default function FirstSection() {
+	return (
+		<div className="firstSection">
+			<div className="top">
+				<div className="left">
+					<h2>WANDIES HAIR AND BEAUTY SALON</h2>
+					<p>
+						Get yourself a makeover or get rejunevated at the Wandies Salond and
+						Spa
+					</p>
+				</div>
+				<div className="rightCard">
+					<a href="#">BOOK APPOINTMENT</a>
+					<p>Complimentary Barista Coffee with all appointments.</p>
+				</div>
+			</div>
+			<div className="lower">
+				<a href="#">SALON</a>
+				<a href="#">SPA</a>
+				<a href="#">OFFERS</a>
+				<a href="#">CONTACT</a>
+			</div>
+		</div>
+	);
+}
