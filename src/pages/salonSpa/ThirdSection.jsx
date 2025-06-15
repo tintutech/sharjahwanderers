2a7c25f455f7d9613a5b16b07c3cf00d40314fc3
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-06-15 10:50:06 +0400

    progress

---
+++ src/pages/salonSpa/ThirdSection.jsx
@@ -0,0 +1,13 @@
+import FAQ from "./FAQ.jsx";
+
+export default function ThirdSection() {
+	return (
+		<div className="thirdSection">
+			<div className="one"></div>
+			<div className="two">
+				<div className="card"></div>
+				<FAQ />
+			</div>
+		</div>
+	);
+}
