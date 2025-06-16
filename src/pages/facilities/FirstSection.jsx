ff397f41d8917dbc838ba99174db4b12f23ecb73
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-06-16 10:23:52 +0400

    FirstSection.jsx

--- src/pages/facilities/FirstSection.jsx
+++ src/pages/facilities/FirstSection.jsx
@@ -1,6 +1,18 @@
 import "./firstSectionStyles.css";
 
-function Announcement() {}
+function Announcement() {
+	return (
+		<div className="announcementCard">
+			<p>
+				<span>ANNOUNCEMENT</span>: Dear member/visitor, kindly note the
+				following facilities will be unavailable from June 10th until further
+				notice.
+			</p>
+			<p>We apologize for the inconvenience.</p>
+			<p className="smallName">The Sharjah Wanderers Sports Club </p>
+		</div>
+	);
+}
 
 function Card() {
 	return (
@@ -17,6 +29,7 @@ function Card() {
 export default function firstSection() {
 	return (
 		<div className="firstSection">
+			<Announcement />
 			<div className="lower">
 				<div className="left">
 					<h3>FACILITIES</h3>
