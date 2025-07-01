827ca6ef52af651311cb47ab1b3b6f66dab28ee4
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-07-01 20:13:46 +0400

    events/Events.jsx

---
+++ src/pages/events/Events.jsx
@@ -0,0 +1,11 @@
+import Nav from "/src/components/Nav.jsx";
+import Footer from "/src/components/Footer.jsx";
+
+export default function Events() {
+	return (
+		<div id="eventsPage">
+			<Nav />
+			<Footer />
+		</div>
+	);
+}
