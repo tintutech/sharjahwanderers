686d13055f208c2f45c25d4242805fc5ba53f7b3
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-07-01 21:48:22 +0400

    offerEvents/OfferEvents.jsx

---
+++ src/pages/offerEvents/OfferEvents.jsx
@@ -0,0 +1,14 @@
+import Nav from "/src/components/Nav.jsx";
+import Footer from "/src/components/Footer.jsx";
+import First from "./First.jsx";
+
+import "./offerEventsStyles.css";
+
+export default function OfferEvents() {
+	return (
+		<div id="offerEvents">
+			<Nav />
+			<Footer />
+		</div>
+	);
+}
