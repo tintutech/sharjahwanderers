14088333827bc5f4bb1eff4169937a060b97ba4a
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-07-01 21:48:03 +0400

    offerEvents/First.jsx

---
+++ src/pages/offerEvents/First.jsx
@@ -0,0 +1,18 @@
+export default function First() {
+	return (
+		<div className="first">
+			<div className="left">
+				<h2>EVENTS</h2>
+				<p>Our clubhouse and sports facilities host events frequently.</p>
+				<div className="bottomLinks">
+					<a href="#">EVENTS</a>
+					<a href="#">OFFERS</a>
+				</div>
+			</div>
+			<div className="right">
+				<p>Interested in hosting your event at one of our venues?</p>
+				<a href="#">Get in touch.</a>
+			</div>
+		</div>
+	);
+}
