a1f0abd3e4e524dd21e79c75e44ef00672de78cd
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-07-10 18:36:31 +0400

    Second.jsx

--- src/pages/offerEvents/Second.jsx
+++ src/pages/offerEvents/Second.jsx
@@ -1,3 +1,37 @@
+import image1 from "/offerEvents/image1.jpg";
+
+let data = [
+	{
+		img: image1,
+		month: "MAY",
+		date: "04",
+		title: "May the 4th be with you.",
+		cardContent: [
+			"May the 4th Be with you.",
+
+			"Star wars themed night. At the Wanderers sports bar.",
+		],
+		link: "#",
+	},
+];
+
+function Cards() {
+	let returnImages = images.map((e) => {
+		console.log(e);
+		return <div className="card"></div>;
+	});
+}
+
 export default function Second() {
-	return <div className="second"></div>;
+	return (
+		<div className="second">
+			<div className="top">
+				<h2>UPCOMING EVENTS</h2>
+				<a href="#">CALENDAR</a>
+			</div>
+			<div className="cardContainer">
+				<Cards />
+			</div>
+		</div>
+	);
 }
