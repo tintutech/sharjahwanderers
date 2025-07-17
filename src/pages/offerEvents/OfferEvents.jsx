c8bf8fd6ee1fcd99cbf42a798f33d3029a08393e
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-07-17 13:50:22 +0400

    OfferEvents.jsx

--- src/pages/offerEvents/OfferEvents.jsx
+++ src/pages/offerEvents/OfferEvents.jsx
@@ -2,6 +2,7 @@ import Nav from "/src/components/Nav.jsx";
 import Footer from "/src/components/Footer.jsx";
 import First from "./First.jsx";
 import Second from "./Second.jsx";
+import Third from "./Third.jsx";
 
 import "./offerEventsStyles.css";
 
