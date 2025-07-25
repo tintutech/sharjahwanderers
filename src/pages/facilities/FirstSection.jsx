f736dcd803e0cfaf23935dedd3fd40c23f15dc01
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-07-25 18:27:13 +0400

    FirstSection.jsx

--- src/pages/facilities/FirstSection.jsx
+++ src/pages/facilities/FirstSection.jsx
@@ -39,7 +39,7 @@ export default function firstSection() {
 					</p>
 					<div className="links">
 						<a href="#">FACILITIES</a>
-						<a href="#">TRAINING</a>
+						<a href="#training">TRAINING</a>
 					</div>
 				</div>
 				<Card />
