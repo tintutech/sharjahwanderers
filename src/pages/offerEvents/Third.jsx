c4da090efb7e3c719d932e9077f455d84fa20ceb
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-07-25 19:09:17 +0400

    progress

--- src/pages/offerEvents/Third.jsx
+++ src/pages/offerEvents/Third.jsx
@@ -61,7 +61,7 @@ export default function Third() {
 				<Cards />
 				<Cards />
 			</div>
-			<div className="membershipFAQ">
+			<div className="memFaq">
 				<MembershipCard />
 				<FAQ />
 			</div>
