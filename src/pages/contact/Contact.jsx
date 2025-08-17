7bd261cc0b30bd5401fb616091b5e91b4a4fff20
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-08-17 15:04:36 +0400

    Contact.jsx

---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,12 @@
+import Nav from "/src/components/Nav.jsx";
+import Footer from "/src/components/Footer.jsx";
+import "./contactStyles.css";
+
+export default function Contact() {
+	return (
+		<div id="contact">
+			<Nav />
+			<Footer />
+		</div>
+	);
+}
