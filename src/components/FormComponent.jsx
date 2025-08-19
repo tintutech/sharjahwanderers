a00b4b090d68db44a4991f46f49173bbd94a87ad
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-08-19 15:02:23 +0400

    FormComponent.jsx

---
+++ src/components/FormComponent.jsx
@@ -0,0 +1,23 @@
+export default function FormComponent() {
+	return (
+		<form action="" method="POST" className="form">
+			<input type="text" name="name" placeholder="NAME" />
+			<input type="tel" name="phone" placeholder="PHONE" />
+			<input type="mail" name="email" placeholder="E-MAIL" />
+			<input type="text" name="subject" placeholder="SUBJECT" />
+			<select name="options">
+				<option value="">INQUIRY TYPE</option>
+				<option value="option1">option 1</option>
+				<option value="option2">option 2</option>
+				<option value="option3">option 3</option>
+				<option value="option4">option 4</option>
+				<option value="option5">option 5</option>
+			</select>
+			<textarea
+				name="noteMessage"
+				placeholder="NOTE MESSAGE (200 WORDS)"
+			></textarea>
+			<button type="submit">SEND INQUIRY</button>
+		</form>
+	);
+}
