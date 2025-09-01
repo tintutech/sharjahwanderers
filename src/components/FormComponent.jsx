280e71b0d54db4a0aab824ae791509e4a1949dd0
Tintu Thomas <tintu.markose@adcustoms.gov.ae>
2025-09-02 00:13:36 +0400

    Update contact form

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,25 +1,75 @@
-import "./formComponentStyles.css";
+import Swal from 'sweetalert2';
+// import "./formComponentStyles.css";
+import "./Contact.css";
 
 export default function FormComponent() {
+	const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "38e7f6db-2af6-4ec2-905a-a846e6ff1d9f");
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        const res = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: json
+        }).then((res) => res.json());
+
+        if (res.success) {
+            Swal.fire({
+                title: "Success!",
+                text: "Message sent successfully!",
+                icon: "success"
+            });
+        }
+    };
 	return (
-		<form action="new_page.json" target="_blank" method="POST" className="form">
-			<input type="text" name="name" placeholder="NAME" />
-			<input type="tel" name="phone" placeholder="PHONE" />
-			<input type="mail" name="email" placeholder="E-MAIL" />
-			<input type="text" name="subject" placeholder="SUBJECT" />
-			<select name="options">
-				<option value="">INQUIRY TYPE</option>
-				<option value="option1">option 1</option>
-				<option value="option2">option 2</option>
-				<option value="option3">option 3</option>
-				<option value="option4">option 4</option>
-				<option value="option5">option 5</option>
-			</select>
-			<textarea
-				name="noteMessage"
-				placeholder="NOTE MESSAGE (200 CHARACTERS)"
-			></textarea>
-			<button type="submit">SEND INQUIRY</button>
-		</form>
+		// <form action="new_page.json" target="_blank" method="POST" className="form">
+		// 	<input type="text" name="name" placeholder="NAME" />
+		// 	<input type="tel" name="phone" placeholder="PHONE" />
+		// 	<input type="mail" name="email" placeholder="E-MAIL" />
+		// 	<input type="text" name="subject" placeholder="SUBJECT" />
+		// 	<select name="options">
+		// 		<option value="">INQUIRY TYPE</option>
+		// 		<option value="option1">option 1</option>
+		// 		<option value="option2">option 2</option>
+		// 		<option value="option3">option 3</option>
+		// 		<option value="option4">option 4</option>
+		// 		<option value="option5">option 5</option>
+		// 	</select>
+		// 	<textarea
+		// 		name="noteMessage"
+		// 		placeholder="NOTE MESSAGE (200 CHARACTERS)"
+		// 	></textarea>
+		// 	<button type="submit">SEND INQUIRY</button>
+		// </form>
+		<section className="contactUS">
+			<form onSubmit={onSubmit}>
+				<h2>Contact Form</h2>
+				<div className="input-box">
+					<label>Full Name</label>
+					<input type="text" className="field" placeholder='Enter your name' name='name' required />
+				</div>
+				<div className="input-box">
+					<label>Mobile Number</label>
+					<input type="text" className="field" placeholder='Enter your mobile' name='mobile' required />
+				</div>
+				<div className="input-box">
+					<label>Email Address</label>
+					<input type="email" className="field" placeholder='Enter your email' name='email' required />
+				</div>
+				<div className="input-box">
+					<label>Your Message</label>
+					<textarea name="message" className="field mess" placeholder='Enter your message' required></textarea>
+				</div>
+				<button type="submit">Send Message</button>
+			</form>
+		</section>
 	);
 }
---
+++ src/pages/contact/FirstSection.jsx
@@ -0,0 +1,27 @@
+import "./firstSectionStyles.css";
+
+export default function FirstSection() {
+	return (
+		<div className="firstSection">
+			<div className="top">
+				<div className="left">
+					<h2>WANDIES HAIR AND BEAUTY SALON</h2>
+					<p>
+						Get yourself a makeover or get rejunevated at the Wandies Salon and
+						Spa
+					</p>
+				</div>
+				<div className="right">
+					<a href="#">BOOK APPOINTMENT</a>
+					<p>Complimentary Barista Coffee with all appointments.</p>
+				</div>
+			</div>
+			<div className="lower">
+				<a href="#">SALON</a>
+				<a href="#">SPA</a>
+				<a href="#">OFFERS</a>
+				<a href="#">CONTACT</a>
+			</div>
+		</div>
+	);
+}
