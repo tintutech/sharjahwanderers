eec64b0ef889efe58b4d2b4b93a0345a55c62b7a
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-09-11 15:49:22 +0400

    progress

--- src/pages/offerEvents/Second.jsx
+++ src/pages/offerEvents/Second.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import image1 from "/offerEvents/image1.jpg";
 import "./secondStyles.css";
 import { getEventCards } from "/src/contentful.js";
+import CTA from "./EventsCTA.jsx";
 
 // getEventCards().then((e) => console.log(e));
 
@@ -98,7 +99,7 @@ export default function Second() {
 		<div className="second">
 			<div className="top">
 				<h2>UPCOMING EVENTS</h2>
-				<a href="#">ATTEND EVENT</a>
+				<a onClick={CTA}>ATTEND EVENT</a>
 			</div>
 			<div className="cardContainer">
 				<Cards content={eventCards} loading={isLoading} />
