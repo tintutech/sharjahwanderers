b241cd14cd53b70449edf109d3014905a11f24da
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-09-12 18:48:20 +0400

    added scrollwheel

--- src/HeroNav.jsx
+++ src/HeroNav.jsx
@@ -1,28 +1,41 @@
 import "./heroStyles.css";
 import Nav from "./components/Nav.jsx";
 import CTA from "./components/CTA.jsx";
+import "./mouseScroll.css";
 
 function HeroSection() {
 	return (
-		<div className="heroSection">
-			<div className="heroLeft">
-				<h2>
-					A private{" "}
-					<span className="heroSpecial">
-						<em>Oasis</em>
-					</span>
-					, for all wanderers in Sharjah.
-				</h2>
-				<p>Be part of the club, become a member today!</p>
-				<div className="bg-blur">
-					<a onClick={CTA}>Become a member</a>
+		<>
+			<div className="heroSection">
+				<div className="heroLeft">
+					<h2>
+						A private{" "}
+						<span className="heroSpecial">
+							<em>Oasis</em>
+						</span>
+						, for all wanderers in Sharjah.
+					</h2>
+					<p>Be part of the club, become a member today!</p>
+					<div className="bg-blur">
+						<a onClick={CTA}>Become a member</a>
+					</div>
+					<div className="bg-blur">
+						<a href="events">Explore offers, or attend an event?</a>
+					</div>
 				</div>
-				<div className="bg-blur">
-					<a href="events">Explore offers, or attend an event?</a>
+				<div className="heroRight"></div>
+			</div>
+			<div className="mouse_scroll">
+				<div className="mouse">
+					<div className="wheel"></div>
+				</div>
+				<div>
+					<span className="m_scroll_arrows unu"></span>
+					<span className="m_scroll_arrows doi"></span>
+					<span className="m_scroll_arrows trei"></span>
 				</div>
 			</div>
-			<div className="heroRight"></div>
-		</div>
+		</>
 	);
 }
 
