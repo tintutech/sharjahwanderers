a0d195e3e2eef0e303d6d244f0901dd4f52cf41e
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-09-14 03:38:59 +0400

    changed api key to client's

--- src/contentful.js
+++ src/contentful.js
@@ -1,9 +1,14 @@
 import { createClient } from "contentful";
 const client = createClient({
+	space: "148elm5iup71",
+	accessToken: "wVL0ppsUv7aZSBmetAk0-U0c9ZhkToM3hYN6yWmiunU",
+});
+/*
 	space: "aoazvbgqcfuv",
 	accessToken: "rA_4yp3MscdCULtaK48V-wKJff6Bdkxgcvn0ztW2v9g",
 });
 
+*/
 export const getPromotionalCards = () =>
 	client
 		.getEntries({ content_type: "promotions" })
@@ -16,12 +21,12 @@ export const getEventCards = () =>
 
 export const getRestaurantOffers = () =>
 	client
-		.getEntries({ content_type: "restaurantOffers"})
+		.getEntries({ content_type: "restaurantOffers" })
 		.then((response) => response.items);
 
 export const getCarouselImages = () =>
 	client
-		.getEntries({ content_type: "cateringCarousel"})
+		.getEntries({ content_type: "cateringCarousel" })
 		.then((response) => response.items);
 
 export const getImage = (sysId) =>
