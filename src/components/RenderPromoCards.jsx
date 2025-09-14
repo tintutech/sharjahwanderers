19d34015e94228ad27f9a5bc2e31165daa874a37
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-09-14 10:47:56 +0400

    RenderPromoCards.jsx

--- src/components/RenderPromoCards.jsx
+++ src/components/RenderPromoCards.jsx
@@ -3,22 +3,20 @@ import "./RenderPromoCards.css";
 
 export const RenderPromoCards = ({ promoCards, isLoading }) => {
 	if (isLoading) return <p>Loading...</p>;
+	console.log(promoCards[0].fields);
 	return (
 		<div className="cardContainerPromo">
 			{promoCards?.map((card, index) => (
 				<div key={index} className="card">
 					<img
-						src={
-							card?.fields?.promoImage?.fields?.file?.url ||
-							card?.fields?.promoTitle
-						}
-						alt={card.fields?.promoImage?.fields?.title}
+						src={card?.fields?.image?.fields?.file?.url || card?.fields?.title}
+						alt={card.fields?.image?.fields?.title}
 					/>
 					<div className="lowerCard">
-						<h3>{card?.fields?.promoTitle}</h3>
+						<h3>{card?.fields?.title}</h3>
 						<div
 							dangerouslySetInnerHTML={{
-								__html: documentToHtmlString(card?.fields?.promoDescription),
+								__html: documentToHtmlString(card?.fields?.desc),
 							}}
 						/>
 					</div>
