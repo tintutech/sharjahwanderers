6c91d1da6e1aa42dee74c25f35dc2a33ea2afe99
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-09-16 05:12:28 +0400

    Announcements component

---
+++ src/components/Announcements.jsx
@@ -0,0 +1,33 @@
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import { getAnnouncements } from "/src/contentful.js";
+import { useState, useEffect } from "react";
+
+export default function Announcement({ pageTitle }) {
+	let [info, setInfo] = useState("");
+
+	useEffect(() => {
+		getAnnouncements().then((e) => {
+			e.map((e) => {
+				if (e.fields?.title === pageTitle) {
+					setInfo((info = e));
+				}
+			});
+		});
+	}, []);
+	if (info != false) {
+		console.log(info);
+		return (
+			<div className="announcementCard">
+				<p>
+					<span>ANNOUNCEMENT:</span>
+				</p>
+				<div
+					dangerouslySetInnerHTML={{
+						__html: documentToHtmlString(info.fields?.announcementField),
+					}}
+				/>
+				<p className="smallName">The Sharjah Wanderers Sports Club </p>
+			</div>
+		);
+	}
+}
