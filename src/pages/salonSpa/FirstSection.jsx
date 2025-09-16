ebd14004c5cd673af7b31b4da44f00e2a8e6a2bb
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-09-16 06:06:38 +0400

    added contenful data to salonSpa

--- src/pages/salonSpa/FirstSection.jsx
+++ src/pages/salonSpa/FirstSection.jsx
@@ -1,6 +1,7 @@
 import "./firstSectionStyles.css";
 
 import Carousel from "../../components/Carousel.jsx";
+import { useState, useEffect } from "react";
 
 import image1 from "/offers/image1.jpg";
 import image2 from "/offers/image2.png";
@@ -9,10 +10,24 @@ import image4 from "/offers/image4.jpg";
 import image5 from "/offers/image5.jpg";
 import image6 from "/offers/image6.jpg";
 import image7 from "/offers/image7.jpg";
+import { getSalonImages } from "/src/contentful.js";
 
 let images = [image1, image2, image3, image4, image5, image6, image7];
 
 export default function FirstSection() {
+	let [loading, setLoading] = useState(true);
+	let [imageList, setImgList] = useState([]);
+	useEffect(() => {
+		let img = [];
+		getSalonImages().then((data) => {
+			console.log(data);
+			data.map((e) => img.push(e.fields?.image?.fields?.file?.url));
+			setImgList((imageList = img));
+			setLoading(false);
+			console.log(imageList);
+		});
+	}, []);
+
 	return (
 		<div className="firstSection">
 			<div className="top">
@@ -24,7 +39,7 @@ export default function FirstSection() {
 					</p>
 				</div>
 				<div className="right">
-					<Carousel img={images} />
+					<Carousel img={imageList} loading={loading} />
 					<div className="smallCard">
 						<a
 							href="https://www.fresha.com/a/wandies-hair-and-beauty-salon-sharjah-sharjah-wanderers-sports-club-bvv369l7"
